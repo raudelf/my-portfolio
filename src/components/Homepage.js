e0d1f93ae1a588ec9cdac37c95982d1849ecd845
raudelf <rawdell@outlook.com>
2020-03-28 18:17:08 -0500

    Got a few more things typed in

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Homepage = () => {
     return (
         <div className='homepageContainer'>
-
+            <header className='headline'>
+                <h1>Raudel Flores</h1>
+                <h3>Front End Engineer</h3>
+            </header>
+            <section className=''>
+                
+            </section>
         </div>
     )
 };
