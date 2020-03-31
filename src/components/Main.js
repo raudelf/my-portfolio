ddcf0686b302a56fc7c3ba15621cfe2f01a64af0
raudelf <rawdell@outlook.com>
2020-03-31 12:46:50 -0500

    renamed from homepage to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const Homepage = () => {
     return (
-        <div className='homepageContainer'>
+        <div className='mainContainer' id='main'>
             <header className='headline'>
                 <h1>Raudel Flores</h1>
                 <h3>Front End Engineer</h3>
             </header>
-            <section className=''>
-                
-            </section>
         </div>
     )
 };
