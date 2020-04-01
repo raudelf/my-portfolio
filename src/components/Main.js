551dde343838f812c1a8612acb54d044704fd155
raudelf <rawdell@outlook.com>
2020-04-01 12:02:23 -0500

    Added in my summary and applied some classnames for styling later

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,15 @@ const Homepage = () => {
     return (
         <div className='mainContainer' id='main'>
             <header className='headline'>
-                <h1>Raudel Flores</h1>
-                <h3>Front End Engineer</h3>
+                <div className='title mainTitle'>
+                    <h1>Raudel Flores</h1>
+                    <h3>Front End Engineer</h3>
+                </div>
+                <div className='summary'>
+                    <p>
+                    With a background in graphic design and love for computers, I am eager to transition into a software engineer role. My interest first sparked in high-school, but it was difficult to find an affordable school with a good web development program. With the help of Lambda School and my design skills, I’m eager to develop functional and clean-looking apps for end-users.
+                    </p>
+                </div>
             </header>
         </div>
     )
