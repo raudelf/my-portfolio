b4657638c1c108cda094fb1e438cffccf0246a6c
raudelf <rawdell@outlook.com>
2020-04-01 12:01:23 -0500

    started laying out my achievements info

---
+++ src/components/Achievements.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Achievements = () => {
+    return (
+        <div className='achievementsContainer' id='achievements'>
+            <div className='sectionHeadline'>
+                <h1>Achievements</h1>
+            </div>
+            <section className='xCulture'>
+                <div className='title eduTitle'>
+                    <h2>2019 X-Culture Award</h2>
+                    <h3>Certificate of Global Collaboration &amp; Best Team Award </h3>
+                </div>
+                <div className='summary'>
+                    <p>
+                        The X-culture Global Collaboration course involved theoretical training and two months of practical experience as a member of a global virtual team. My team developed a high-quality business plan for a multinational company, BeeUp, working alongside over 5,000 students from 130 universities in over 40 countries on 6 continents.
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Achievements;
