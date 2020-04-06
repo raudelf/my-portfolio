c0e653e1148ee04cdff2df7c7f6109503609ef66
raudelf <rawdell@outlook.com>
2020-04-05 23:13:27 -0500

    created the foodie fun component

--- src/components/projects/Foodiefun.js
+++ src/components/projects/Foodiefun.js
@@ -4,7 +4,7 @@ const Foodiefun = () => {
     return (
         <section className='foodieFun'>
                 <div className='projImg'>
-                        <img href='https://raudelf.github.io/Marketing-Page/' src='./assets/foodie-fun.jpg' alt='Snapshot of a website I worked on'  />
+                    <img href='https://raudelf.github.io/Marketing-Page/' src='./assets/foodie-fun.jpg' alt='Snapshot of a website I worked on'  />
                 </div>
                 <h2>FoodieFun</h2>
                 <p>A marketing landing page built in collaboration with a UX Designer. This was one of Raudel's first collaboration projects with a dev team.</p>
