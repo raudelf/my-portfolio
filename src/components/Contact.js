c4aa25bef0412c3edcff8bd94f9f72dd923c8743
raudelf <rawdell@outlook.com>
2020-04-06 14:58:20 -0500

    finished laying out the Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='container contactContainer' id='contact'>
+            <section className='sectionHeadline'>
+                <h1>Get in Touch!</h1>
+            </section>
+            <form>
+                <h2>... This Section is under construction ...</h2>
+                <p>In the meantime, hit the button below to shoot me an email. I look forward to speaking with you!</p>
+                <button><a href='mailto:rawdell@outlook.com'>Email</a></button>
+            </form>
+
+        </div>
+    );
+};
+
+export default Contact;
