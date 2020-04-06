f4a5a41e0ef70a3e4def58aec443ec4d5571dcfd
raudelf <rawdell@outlook.com>
2020-04-05 22:57:32 -0500

    created a folder that contains all projects

---
+++ src/components/projects/Foodiefun.js
@@ -0,0 +1,15 @@
+import React from  'react';
+
+const Foodiefun = () => {
+    return (
+        <section className='foodieFun'>
+                <div className='projImg'>
+                        <img href='https://raudelf.github.io/Marketing-Page/' src='./assets/foodie-fun.jpg' alt='Snapshot of a website I worked on'  />
+                </div>
+                <h2>FoodieFun</h2>
+                <p>A marketing landing page built in collaboration with a UX Designer. This was one of Raudel's first collaboration projects with a dev team.</p>
+        </section>
+    );
+};
+
+export default Foodiefun;
