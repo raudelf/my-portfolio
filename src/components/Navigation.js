5e6a72a3b9d1fd870e60da57ed50f2a850f200b1
raudelf <rawdell@outlook.com>
2020-08-05 21:56:45 -0500

    :fire: Removing Imports / Code
    Got rid of the code importing the deleted components and removed the links from the navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,60 +1,45 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 
 const Navigation = () => {
-    return(
-        <div className='navContainer'>
-            <nav className='nav'>
-                <Link
-                activeClass='active'
-                to='main'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Main</Link>
+  return (
+    <div className="navContainer">
+      <nav className="nav">
+        <Link
+          activeClass="active"
+          to="main"
+          spy={true}
+          smooth={true}
+          offset={-60}
+          duration={500}
+        >
+          Main
+        </Link>
 
-                <Link
-                activeClass='active'
-                to='projects'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Projects</Link>
+        <Link
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={-60}
+          duration={500}
+        >
+          Projects
+        </Link>
 
-                <Link
-                activeClass='active'
-                to='education'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Education</Link>
-
-                <Link
-                activeClass='active'
-                to='skills'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Skills</Link>
-
-                <Link
-                activeClass='active'
-                to='achievements'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Achievements</Link>
-
-                <Link
-                activeClass='active'
-                to='contact'
-                spy= {true}
-                smooth= {true}
-                offset= {-60}
-                duration= {500}>Contact</Link>
-            </nav>
-        </div>
-    )
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-60}
+          duration={500}
+        >
+          Contact
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
