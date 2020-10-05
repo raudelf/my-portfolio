74868a42eec78db7bd040c8e1bac16426b80ef2b
Raudel Flores <rawdell@outlook.com>
2020-10-04 19:13:55 -0500

    :sparkles: EmailJS
    Implemented EmailJS on my contact section which allows users to send em an email directly from my portfolio. This saves time and removes the hassle of them having to copy my email and go to their email provider to send it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, {useState, setState} from 'react';
+import * as emailjs from "emailjs-com";
+
 
 const Contact = () => {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        body: ""
+    })
+
+    const [sent, setSent] = useState(false)
+
+    const handleChanges = e => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setSent(true)
+
+        const templateParams = {
+            from_name: `${form.name} (${form.email})`,
+            to_name: 'raudelf3@gmail.com',
+            message: form.body
+        }
+
+        emailjs
+            .send(
+                'service_0q40sjk',
+                'template_q4e3eax',
+                templateParams,
+                'user_H4oGojaVAorpxbCZjGArT',
+            )
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+        setForm({...form,
+        name: "",
+        email: "",
+        body: ""
+        })
+    }
+
     return (
         <div className='container contactContainer' id='contact'>
             <section className='sectionHeadline'>
                 <h1>Get in Touch!</h1>
             </section>
-            <form>
-                <h2>... This Section is under construction ...</h2>
-                <p>In the meantime, hit the button below to shoot me an email. I look forward to speaking with you!</p>
-                <button><a href='mailto:rawdell@outlook.com'>Email</a></button>
+            <form className="formContainer" onSubmit={handleSubmit}>
+                <h2>Send an Email</h2>
+                <input 
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                value={form.name}
+                onChange={handleChanges}
+                />
+                <input 
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                value={form.email}
+                onChange={handleChanges}
+                />
+                <textarea 
+                name="body"
+                placeholder="Message"
+                value={form.body}
+                onChange={handleChanges}
+                />
+                <button className="emailBtn">Send Email</button>
             </form>
 
         </div>
