6074130879ac2bf1bb45f9202e79bd7df1d59626
raudelf <rawdell@outlook.com>
2021-01-18 23:39:00 -0600

    :sparkles: Contact Form
    Finally set up my contact form and is functioning properly! Emails are sent to my email and users get an automatic email as well.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const [err, setErr] = useState(false)
+    const [form, setForm] = useState({
+        from_name: '',
+        reply_to: '',
+        message: ''
+    })
+
+    const handleChange = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        emailjs.send('service_0q40sjk', 'template_q4e3eax', form, 'user_H4oGojaVAorpxbCZjGArT')
+        .then(res => {
+            console.log('SUCCESS: ', res)
+            setForm({
+                from_name: '',
+                reply_to: '',
+                message: ''
+            })
+        })
+        .catch(res => {
+            console.log('ERROR SENDING EMAIL: ', res)
+        })
+    }
     return (
         <div className='container contactContainer' id='contact'>
             <section className='sectionHeadline'>
                 <h1>Get in Touch!</h1>
             </section>
-            <form>
-                <h2>... This Section is under construction ...</h2>
-                <p>In the meantime, hit the button below to shoot me an email. I look forward to speaking with you!</p>
-                <button><a href='mailto:rawdell@outlook.com'>Email</a></button>
+            <form className='formContainer'>
+                <input 
+                type='text'
+                placeholder='Name'
+                name='from_name'
+                onChange={handleChange}
+                value={form.from_name}/>
+                <input
+                type='email'
+                placeholder='Email'
+                name='reply_to'
+                onChange={handleChange}
+                value={form.reply_to}
+                />
+                <textarea
+                placeholder='Message'
+                name='message'
+                onChange={handleChange}
+                value={form.message}/>
+                <button className='emailBtn' onClick={handleSubmit}>Send Email</button>
             </form>
 
         </div>
