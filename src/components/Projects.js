e540556c32c9d7d0aa1a838e50fe097e0ec616b6
raudelf <rawdell@outlook.com>
2021-02-16 22:07:06 -0600

    :recycle: Links to Buttons
    Refactored the code to use buttons that update the state and what project is displayed on the page. This fixes the issue of having the url change making it faster

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { NavLink, Route } from "react-router-dom";
+import React, {useState} from "react";
 
 // Projects
 import FoodieFun from "./projects/Foodiefun";
+import SpotifySS from "./projects/SpotifySS";
+import TallyAi from "./projects/TallyAi";
 
 const Projects = () => {
+  const [proj, setProj] = useState('foodie')
+
   return (
     <div className="container projectsContainer" id="projects">
       <section className="sectionHeadline">
@@ -12,31 +15,20 @@ const Projects = () => {
       </section>
       <section className="splitHalf">
         <div className="projectLinks">
-          <NavLink
-            className="projectBtn"
-            activeClassName="activeProject"
-            to="/foodie-fun"
-          >
-            Foodie fun
-          </NavLink>
-          <NavLink
-            className="projectBtn"
-            activeClassName="activeProject"
-            to="/spotify-song-suggester"
-          >
-            Spotify Song Suggester
-          </NavLink>
-          <NavLink
-            className="projectBtn"
-            activeClassName="activeProject"
-            to="/tally-ai"
-          >
-            Tally Ai
-          </NavLink>
+          <button className='projectBtn' onClick={() => {
+            setProj('foodie')
+          }}>Foodie Fun</button>
+          <button className='projectBtn' onClick={() => {
+            setProj('spotify')
+          }}>Spotify Song Suggester</button>
+          <button className='projectBtn' onClick={() => {
+            setProj('tally')
+          }}>Tally Ai</button>
         </div>
-        <div className="projectRoutes">
-          <Route exact path="/foodie-fun" component={FoodieFun} />
-          <Route path="/spotify-song-suggester" />
+        <div className='projectRoutes'>
+          <FoodieFun proj={proj}/>
+          <SpotifySS proj={proj}/>
+          <TallyAi proj={proj} />
         </div>
       </section>
     </div>
